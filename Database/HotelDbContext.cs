80d31ec73a25748b4a9eb640406a92c3585b6e7b
VitorNuness <vitor1nuness@gmail.com>
2024-09-18 20:33:48 -0300

    inicializando sistema

---
+++ Database/HotelDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Database;
+
+public partial class HotelDbContext : DbContext
+{
+    public HotelDbContext()
+    {
+    }
+
+    public HotelDbContext(DbContextOptions<HotelDbContext> options)
+        : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
