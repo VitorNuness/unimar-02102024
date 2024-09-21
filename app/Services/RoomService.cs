0f8ae3a377bf642813f3c53ebf91dbe2059afed5
VitorNuness <vitor1nuness@gmail.com>
2024-09-20 21:50:22 -0300

    adicionando método de buscar usuário no RoomService

--- app/Services/RoomService.cs
+++ app/Services/RoomService.cs
@@ -2,11 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hotel.app.Models;
+using Hotel.app.Repositories;
 
 namespace Hotel.app.Services
 {
     public class RoomService
     {
-        
+        private readonly RoomRepository _roomRepository;
+
+        public RoomService(
+            RoomRepository roomRepository
+        )
+        {
+            _roomRepository = roomRepository;
+        }
+
+        public async Task<Room> Get(string id)
+        {
+            return await _roomRepository.FindOrFail(id);
+        }
     }
 }
