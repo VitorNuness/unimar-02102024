1565db605c50e5a64e00b21fa1066fff6d43ff42
VitorNuness <vitor1nuness@gmail.com>
2024-09-20 21:32:12 -0300

    adicionando método para salvar a reserva no banco de dados

--- app/Repositories/ReserveRepository.cs
+++ app/Repositories/ReserveRepository.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hotel.app.Models;
+using Hotel.Database;
 
 namespace Hotel.app.Repositories
 {
     public class ReserveRepository
     {
-        
+        private readonly HotelDbContext _dbContext;
+
+        public ReserveRepository(
+            HotelDbContext dbContext
+        )
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Reserve> Store(Reserve reserve)
+        {
+            _dbContext.Reserves.Add(reserve);
+            await _dbContext.SaveChangesAsync();
+
+            return reserve;
+        }
     }
 }
